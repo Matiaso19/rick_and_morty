33f077ce7bbce3aa29bac15667ce1c7a97cb3875
unknown <matiasscrog@gmail.com>
2023-07-11 20:42:45 -0300

    bdd parte 1

--- Server/src/controllers/handleFavorites.js
+++ Server/src/controllers/handleFavorites.js
@@ -8,12 +8,12 @@ function postFav(req, res) {
 
 function deleteFav(req, res) {
     const {id} = req.params;
-    console.log(myFavorites);
-    console.log(typeof(id));
+    
+    
     myFavorites = myFavorites.filter((char) => char.id != id)
     
     return res.status(200).json(myFavorites)
-    console.log(myFavorites);
+    
 }
 
 module.exports = {
