42ca1af87a2605e212687ed86832ccb33a747d59
unknown <matiasscrog@gmail.com>
2023-07-14 20:28:01 -0300

    force en false

--- Server/src/index.js
+++ Server/src/index.js
@@ -2,7 +2,7 @@ const server = require('./app');
 const PORT = 3001;
 const { conn } = require('../DB_connection')
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
    server.listen(PORT, () => console.log('Server raised in port: ' + PORT));
    
 });
